2d6893af56a5d4bf2cf47e290881ea53c8816c7c
Isabella Oliveira <isabellaoliveira12s2@gmail.com>
2025-08-30 10:17:56 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+public abstract class Conta {
+    private String numero;
+    private double saldo;
+    private String titular;
 
-public class Conta {
+    public Conta(String numero, String titular) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = 0.0;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
+
+    public abstract void sacar(double valor);
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero='" + numero + '\'' +
+                ", titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }

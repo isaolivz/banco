0aa7b1f329e3676dca975cd63d239addfb010e74
Isabella Oliveira <isabellaoliveira12s2@gmail.com>
2025-08-30 10:18:28 -0300

    Update Banco.java

--- src/Banco.java
+++ src/Banco.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
 
 public class Banco {
+    private List<Conta> contas;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+        System.out.println("Conta adicionada com sucesso.");
+    }
+
+    public Optional<Conta> buscarConta(String numero) {
+        return contas.stream()
+                .filter(c -> c.getNumero().equals(numero))
+                .findFirst();
+    }
+
+    public void exibirContas() {
+        if (contas.isEmpty()) {
+            System.out.println("Nenhuma conta cadastrada.");
+            return;
+        }
+        System.out.println("--- Lista de Contas ---");
+        contas.forEach(System.out::println);
+        System.out.println("-----------------------");
+    }
 }
